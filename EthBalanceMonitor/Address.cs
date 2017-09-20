da93ef1bb323f91c4aa2837159f8a4fdad5eb51f
valiant1x <valiant1x@users.noreply.github.com>
2017-09-20 16:44:43 -0400

    Initial commit

---
+++ EthBalanceMonitor/Address.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EthBalanceMonitor
+{
+    public class Address : INotifyPropertyChanged
+    {
+        string _Addr;
+        public string Addr
+        {
+            get { return _Addr; }
+            set
+            {
+                _Addr = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        bool _AuditPassed;
+        public bool AuditPassed
+        {
+            get { return _AuditPassed; }
+            set
+            {
+                _AuditPassed = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        float _BalEth;
+        public float BalEth
+        {
+            get { return _BalEth; }
+            set
+            {
+                _BalEth = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        float _BalTnt;
+        public float BalTnt
+        {
+            get { return _BalTnt; }
+            set
+            {
+                _BalTnt = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+        DateTime _LastUpdated;
+        public DateTime LastUpdated
+        {
+            get { return _LastUpdated; }
+            set
+            {
+                _LastUpdated = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Address(string addr)
+        {
+            Addr = addr;
+        }
+    }
+}

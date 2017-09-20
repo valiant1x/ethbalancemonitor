75ac251494099ca03ef7414e9f9954527f3720e5
valiant1x <valiant1x@users.noreply.github.com>
2017-09-20 16:44:43 -0400

    Initial commit

---
+++ EthBalanceMonitor/frmMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace EthBalanceMonitor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgMain = new System.Windows.Forms.DataGridView();
+            this.tmrCheckBalance = new System.Windows.Forms.Timer(this.components);
+            this.txtLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgMain
+            // 
+            this.dgMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMain.Location = new System.Drawing.Point(0, 141);
+            this.dgMain.Name = "dgMain";
+            this.dgMain.Size = new System.Drawing.Size(750, 570);
+            this.dgMain.TabIndex = 0;
+            this.dgMain.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgMain_CellFormatting);
+            // 
+            // tmrCheckBalance
+            // 
+            this.tmrCheckBalance.Enabled = true;
+            this.tmrCheckBalance.Tick += new System.EventHandler(this.tmrCheckBalance_Tick);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.BackColor = System.Drawing.Color.Black;
+            this.txtLog.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(750, 142);
+            this.txtLog.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 711);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.dgMain);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmMain";
+            this.Text = "Token Balance Monitor";
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dgMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgMain;
+        private System.Windows.Forms.Timer tmrCheckBalance;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ EthBalanceMonitor/frmMain.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Net;
+
+namespace EthBalanceMonitor
+{
+    public partial class frmMain : Form
+    {
+        BindingList<Address> Addresses = new BindingList<Address>();
+        List<string> AuditSites = new List<string>()
+        {
+            "https://a.chainpoint.org/",
+            "https://b.chainpoint.org/",
+            "https://c.chainpoint.org/"
+        };
+
+        public frmMain()
+        {
+            InitializeComponent();
+            
+            if (File.Exists("address.txt"))
+            {
+                try
+                {
+                    var lines = File.ReadAllLines("address.txt");
+                    foreach(var line in lines)
+                    {
+                        if (string.IsNullOrEmpty(line) || line.Trim().Length != 42)
+                            continue;
+
+                        if (Addresses.Where(x => x.Addr.Equals(line.Trim(), 
+                            StringComparison.OrdinalIgnoreCase)).Count() == 0)
+                            Addresses.Add(new Address(line.Trim()));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                try
+                {
+                    File.WriteAllText("address.txt", "");
+                }
+                catch { }              
+            }
+
+            AddLogMsg("API graciously powered by ethplorer.io");
+            AddLogMsg("Donations greatly appreciated: 0xa81248aE54dE6521d3afd58848006676EE874bFC");
+            AddLogMsg("Loaded " + Addresses.Count + " addresses to monitor.");
+
+            //bind grid
+            dgMain.DataSource = null;
+            dgMain.AutoGenerateColumns = true;
+            dgMain.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgMain.AllowDrop =
+                dgMain.AllowUserToAddRows =
+                dgMain.AllowUserToDeleteRows =
+                false;
+
+            dgMain.ReadOnly = true;
+
+            dgMain.DataSource = new BindingSource { DataSource = Addresses };
+
+            var _ = CheckTokenBalance();
+        }
+
+        private void tmrCheckBalance_Tick(object sender, EventArgs e)
+        {
+            if (tmrCheckBalance.Interval == 100)
+                tmrCheckBalance.Interval = 15000;
+
+        }
+
+        async Task CheckTokenBalance()
+        {
+            while (true)
+            {
+                bool wasFresh = false;
+
+                try
+                {
+                    //query server
+                    string url = @"https://api.ethplorer.io/getAddressInfo/{ADDR}?apiKey=freekey";
+                    var addr = Addresses.OrderBy(x => x.LastUpdated).First();
+                    url = url.Replace("{ADDR}", addr.Addr);
+
+                    wasFresh = (addr.LastUpdated == new DateTime());
+
+                    HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+                    request.Timeout = 10000;
+                    var response = await request.GetResponseAsync();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string body = await reader.ReadToEndAsync();
+                        dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+
+                        addr.BalEth = jsonResponse.ETH.balance;
+
+                        float newTntBal = jsonResponse.tokens[0].balance / 100000000;
+
+                        if (addr.BalTnt != 0 &&
+                            addr.BalTnt != newTntBal)
+                            AddLogMsg("*** Value changedfor " + addr.Addr + " -- previous: " +
+                                addr.BalTnt + " -- new: " + newTntBal);
+
+                        addr.BalTnt = newTntBal;
+                        addr.LastUpdated = DateTime.Now;
+                    }
+
+                    //check node audit status
+                    string auditUrl = AuditSites[0];
+                    AuditSites.RemoveAt(0);                    
+                    AuditSites.Add(auditUrl);
+
+                    auditUrl += "nodes/" + addr.Addr;
+
+                    request = HttpWebRequest.CreateHttp(auditUrl);
+                    request.Timeout = 10000;
+                    response = await request.GetResponseAsync();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string body = await reader.ReadToEndAsync();
+                        dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+
+                        foreach(dynamic audit in jsonResponse.recent_audits)
+                        {
+                            if (!audit.public_ip_test.ToString().Equals("true", StringComparison.OrdinalIgnoreCase) ||
+                                !audit.time_test.ToString().Equals("true", StringComparison.OrdinalIgnoreCase) ||
+                                !audit.calendar_state_test.ToString().Equals("true", StringComparison.OrdinalIgnoreCase) ||
+                                !audit.minimum_credits_test.ToString().Equals("true", StringComparison.OrdinalIgnoreCase))
+                            {
+                                addr.AuditPassed = false;
+                                AddLogMsg("Audit faile for " + addr.Addr + ". " + body);
+                            }
+                            else
+                                addr.AuditPassed = true;
+
+                            //only check most recent
+                            break;
+                        }
+                    }
+                }
+                catch { }
+                
+                dgMain.AutoResizeColumns();
+
+                await Task.Delay(wasFresh ? 500 : 15000);
+            }
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            if (Addresses.Count == 0)
+            {
+                MessageBox.Show("No addresses found! Put ETH addresses into address.txt file. 1 address per line.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Diagnostics.Process.Start("address.txt");
+
+                this.Close();
+            }
+        }
+
+        void AddLogMsg(string msg)
+        {
+            txtLog.AppendText("[" + DateTime.Now.ToLongTimeString() + "] " + msg + Environment.NewLine);
+        }
+
+        private void dgMain_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dgMain.Columns[e.ColumnIndex].Name == "BalTnt")
+            {
+                Address addr = Addresses[e.RowIndex];
+                if (addr.BalTnt > 3100)
+                {
+                    e.CellStyle.BackColor = Color.Green;
+                    e.CellStyle.ForeColor = Color.WhiteSmoke;
+                }
+            }
+
+            if (dgMain.Columns[e.ColumnIndex].Name == "AuditPassed")
+            {
+                Address addr = Addresses[e.RowIndex];
+                if (!addr.AuditPassed)
+                {
+                    e.CellStyle.BackColor = Color.OrangeRed;
+                    e.CellStyle.ForeColor = Color.WhiteSmoke;
+                }
+            }
+        }
+    }
+}
